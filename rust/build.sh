4cd9992ce2525a527cd3ef65eb55e3b74baf2629
hcchae <hc.chae@sk.com>
2020-05-20 16:50:35 +0900

    first commit

---
+++ rust/build.sh
@@ -0,0 +1,5 @@
+export ANDROID_NDK_HOME=/Users/chaeso/Library/Android/sdk/android-ndk-r21
+cargo ndk --platform 21 --target aarch64-linux-android build
+
+#cargo build --target aarch64-linux-android --release --verbose
+
---
+++ rust/src/lib.rs
@@ -0,0 +1,6 @@
+#![allow(non_snake_case)]
+
+#[no_mangle]
+pub unsafe extern fn helloandroid() {
+}
+
